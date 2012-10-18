cb595b603b2dc14cea56b047344eb193cbee011c
Jean-Baptiste Kempf <jb@videolan.org>
2012-10-18 17:22:19 +0200

    Examples: proper copyright notices

--- Examples/iPodConverter/GradientBackgroundView.h
+++ Examples/iPodConverter/GradientBackgroundView.h
@@ -1,10 +1,24 @@
-//
-//  GradientBackgroundView.h
-//  iPodConverter
-//
-//  Created by Pierre d'Herbemont on 1/12/08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
-//
+/*****************************************************************************
+ * iPodConverter: GradientBackgroundView
+ *****************************************************************************
+ * Copyright (C) 2007-2012 Pierre d'Herbemont and VideoLAN
+ *
+ * Authors: Pierre d'Herbemont
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 
 #import <Cocoa/Cocoa.h>
 
