9230ddbf42ec6376e491876b9047c195cfcdb307
Felix Paul Kühne <fkuehne@videolan.org>
2015-04-01 18:11:55 +0200

    media discoverer: add doxygen documentation

--- Headers/Public/VLCMediaDiscoverer.h
+++ Headers/Public/VLCMediaDiscoverer.h
@@ -2,10 +2,12 @@
  * VLCMediaDiscoverer.h: VLCKit.framework VLCMediaDiscoverer header
  *****************************************************************************
  * Copyright (C) 2007 Pierre d'Herbemont
- * Copyright (C) 2007, 2014 VLC authors and VideoLAN
+ * Copyright (C) 2015 Felix Paul Kühne
+ * Copyright (C) 2007, 2015 VLC authors and VideoLAN
  * $Id$
  *
  * Authors: Pierre d'Herbemont <pdherbemont # videolan.org>
+ *          Felix Paul Kühne <fkuehne # videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -29,20 +31,37 @@
 @class VLCMediaDiscoverer;
 
 /**
- * TODO: Documentation VLCMediaDiscoverer
+ * VLCMediaDiscovererDelegate
  */
 
 @protocol VLCMediaDiscovererDelegate <NSObject>
-
 @optional
 
+/**
+ * delegate method triggered when a discoverer was started
+ *
+ * \param the discoverer that was started
+ */
 - (void)discovererStarted:(VLCMediaDiscoverer *)theDiscoverer;
+
+/**
+ * delegate method triggered when a discoverer was stopped
+ *
+ * \param the discoverer that was stopped
+ */
 - (void)discovererStopped:(VLCMediaDiscoverer *)theDiscoverer;
 
 @end
 
+/**
+ * VLCMediaDiscoverer
+ */
+
 @interface VLCMediaDiscoverer : NSObject
 
+/**
+ * delegate property to listen to start/stop events
+ */
 @property (weak, readwrite) id<VLCMediaDiscovererDelegate> delegate;
 
 /**
@@ -55,23 +74,24 @@
 /* Initializers */
 /**
  * Initializes new object with specified name.
- * \param aSerchName Name of the service for this VLCMediaDiscoverer object.
+ * \param aServiceName Name of the service for this VLCMediaDiscoverer object.
  * \returns Newly created media discoverer.
  */
 - (instancetype)initWithName:(NSString *)aServiceName;
 
 /**
- * TODO: Documentation VLCMediaDiscoverer.discoveredMedia
+ * a read-only property to retrieve the list of discovered media items
  */
 @property (weak, readonly) VLCMediaList *discoveredMedia;
 
 /**
- * TODO: Documentation VLCMediaDiscoverer.localizedName
+ * returns the localized name of the discovery module if available, otherwise in US English
  */
 @property (readonly, copy) NSString *localizedName;
 
 /**
- * TODO: Documentation VLCMediaDiscoverer.isRunning
+ * read-only property to check if the discovery service is active
+ * \return boolean value
  */
 @property (readonly) BOOL isRunning;
 @end
