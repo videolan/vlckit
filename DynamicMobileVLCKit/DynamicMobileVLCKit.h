33e6cc06766b1d162a976079e9bc3ed2690d547f
Felix Paul Kühne <fkuehne@videolan.org>
2015-07-10 17:41:29 +0200

    add basic xcode setup to compile MVK as a dynamic framework

---
+++ DynamicMobileVLCKit/DynamicMobileVLCKit.h
@@ -0,0 +1,37 @@
+//
+//  DynamicMobileVLCKit.h
+//  DynamicMobileVLCKit
+//
+//  Created by Felix Paul Kühne on 10/07/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicMobileVLCKit.
+FOUNDATION_EXPORT double DynamicMobileVLCKitVersionNumber;
+
+//! Project version string for DynamicMobileVLCKit.
+FOUNDATION_EXPORT const unsigned char DynamicMobileVLCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicMobileVLCKit/PublicHeader.h>
+
+#import <DynamicMobileVLCKit/VLCAudio.h>
+#import <DynamicMobileVLCKit/VLCLibrary.h>
+#import <DynamicMobileVLCKit/VLCMedia.h>
+#import <DynamicMobileVLCKit/VLCMediaDiscoverer.h>
+#import <DynamicMobileVLCKit/VLCMediaList.h>
+#import <DynamicMobileVLCKit/VLCMediaPlayer.h>
+#import <DynamicMobileVLCKit/VLCMediaListPlayer.h>
+#import <DynamicMobileVLCKit/VLCMediaThumbnailer.h>
+#import <DynamicMobileVLCKit/VLCTime.h>
+
+@class VLCMedia;
+@class VLCMediaLibrary;
+@class VLCMediaList;
+@class VLCTime;
+@class VLCVideoView;
+@class VLCAudio;
+@class VLCMediaThumbnailer;
+@class VLCMediaListPlayer;
+@class VLCMediaPlayer;
---
+++ StaticLibVLC/StaticLibVLC.m
@@ -0,0 +1,17 @@
+//
+//  StaticLibVLC.m
+//  StaticLibVLC
+//
+//  Created by Felix Paul Kühne on 10/07/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibVLC : NSObject
+
+@end
+
+@implementation StaticLibVLC
+
+@end
