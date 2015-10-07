videolan/vlckit

commit 758cdf2eaa92a3e5e6f957fec8ef42ad2b38f45d

Change property name (Fix KVO notification)

Headers/Public/VLCAudio.h
@@ -38,9 +38,7 @@ extern NSString *const VLCMediaPlayerVolumeChanged;
 @interface VLCAudio : NSObject
 
 /* Properties */
-- (void)setMute:(BOOL)value;
-
-@property (setter=setMute:) BOOL isMuted;
+@property (getter=isMuted) BOOL muted;
 @property (assign) int volume;
 
 - (void)volumeDown;

Sources/VLCAudio.m
@@ -71,7 +71,7 @@ NSString *const VLCMediaPlayerVolumeChanged = @"VLCMediaPlayerVolumeChanged";
     libvlc_media_player_release([self instance]);
 }
 
-- (void)setMute:(BOOL)value
+- (void)setMuted:(BOOL)value
 {
     libvlc_audio_set_mute([self instance], value);
 }

commit 220d46d1abc4f3f7c62325ad2012771e91ba2941

Restore API broken in 758cdf2e with deprecate flag

Headers/Public/VLCAudio.h
@@ -41,6 +41,8 @@ extern NSString *const VLCMediaPlayerVolumeChanged;
 @property (getter=isMuted) BOOL muted;
 @property (assign) int volume;
 
+- (void)setMute:(BOOL)value __attribute__((deprecated));
+
 - (void)volumeDown;
 - (void)volumeUp;
 @end

Sources/VLCAudio.m
@@ -76,6 +76,11 @@ NSString *const VLCMediaPlayerVolumeChanged = @"VLCMediaPlayerVolumeChanged";
     libvlc_audio_set_mute([self instance], value);
 }
 
+- (void)setMute:(BOOL)value
+{
+    [self setMuted:value];
+}
+
 - (BOOL)isMuted
 {
     return libvlc_audio_get_mute([self instance]);