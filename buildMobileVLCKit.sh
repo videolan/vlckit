359ae3275801bacb0fb3f47266ea1f8ef16dbe8f
Jean-Baptiste Kempf <jb@videolan.org>
2017-04-13 17:34:12 +0200

    MVK: Update libvlc HASH

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -21,7 +21,7 @@ OSVERSIONMINCFLAG=miphoneos-version-min
 OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 
-TESTEDHASH=37b7af7
+TESTEDHASH=c203ac56
 
 CORE_COUNT=`sysctl -n machdep.cpu.core_count`
 let MAKE_JOBS=$CORE_COUNT+1
