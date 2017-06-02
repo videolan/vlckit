689cfdec13625fe0c457026f3d4201aa1335a0a7
Thomas Guillem <thomas@gllm.fr>
2017-06-02 16:10:54 +0200

    MVK: fix build
    Fix conflict with cookie patch, and add 0017-core-Fix-TLS-usage.patch

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -21,7 +21,7 @@ OSVERSIONMINCFLAG=miphoneos-version-min
 OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 
-TESTEDHASH=6bc7a637
+TESTEDHASH=f6edc3e
 
 CORE_COUNT=`sysctl -n machdep.cpu.core_count`
 let MAKE_JOBS=$CORE_COUNT+1
