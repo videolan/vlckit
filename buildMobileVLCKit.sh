27425075b297138f63f4e5f736064f739ecf8430
Felix Paul Kühne <fkuehne@videolan.org>
2017-07-20 13:57:45 +0200

    MVK: update libvlc to HEAD

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -21,7 +21,7 @@ OSVERSIONMINCFLAG=miphoneos-version-min
 OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 
-TESTEDHASH=a83f896
+TESTEDHASH=58921590
 
 CORE_COUNT=`sysctl -n machdep.cpu.core_count`
 let MAKE_JOBS=$CORE_COUNT+1
