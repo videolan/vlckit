926b855716f018e4299acb13e1b017a1ce1d2d1d
Felix Paul Kühne <fkuehne@videolan.org>
2017-07-20 18:40:58 +0200

    MVK: remove patches merged upstream and fix compilation

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -21,7 +21,7 @@ OSVERSIONMINCFLAG=miphoneos-version-min
 OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 
-TESTEDHASH=58921590
+TESTEDHASH=b9ca7841
 
 CORE_COUNT=`sysctl -n machdep.cpu.core_count`
 let MAKE_JOBS=$CORE_COUNT+1
