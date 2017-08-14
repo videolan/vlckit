15d4ec7f1d5eb3d0ac72d40d22102ec7e09011f4
Carola Nitz <nitz.carola@googlemail.com>
2017-08-14 15:20:49 +0200

    add VLCAudio header to expose properties also for tvOS

--- Headers/Public/TVVLCKit.h
+++ Headers/Public/TVVLCKit.h
@@ -21,6 +21,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#import <TVVLCKit/VLCAudio.h>
 #import <TVVLCKit/VLCLibrary.h>
 #import <TVVLCKit/VLCMedia.h>
 #import <TVVLCKit/VLCMediaDiscoverer.h>
