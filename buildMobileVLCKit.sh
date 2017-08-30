02493d643730da3767e7b31cf0966cebb00f9a3d
Felix Paul Kühne <felix@serioese.gmbh>
2017-08-30 18:21:36 +0200

    Update libvlc

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -21,7 +21,7 @@ OSVERSIONMINCFLAG=miphoneos-version-min
 OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 
-TESTEDHASH=26e80780
+TESTEDHASH=bb538b4e
 
 CORE_COUNT=`sysctl -n machdep.cpu.core_count`
 let MAKE_JOBS=$CORE_COUNT+1
