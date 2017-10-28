327c414841b3932cd686ebea9545907168cb584b
Felix Paul Kühne <felix@serioese.gmbh>
2017-10-28 17:57:22 +0200

    MVK: update libvlc

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -22,7 +22,7 @@ OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH=e713ded8
+TESTEDHASH=3767a33c
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
