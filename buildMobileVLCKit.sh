89e531e1e41e452659f10b390ad43b406ad95efa
Felix Paul Kühne <felix@serioese.gmbh>
2017-11-12 15:24:29 +0100

    MVK: update libvlc and remove unneeded patch

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -22,7 +22,7 @@ OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH=0885ed8
+TESTEDHASH=ec31390c5
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
