f2d4be922e79ae537613efc4d0f0288e5e31facb
Carola Nitz <nitz.carola@googlemail.com>
2017-11-23 10:36:40 +0100

    bump vlckit tested hash to include playbackfixes for iOS 7 and 8

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -22,7 +22,7 @@ OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH=b6bbde66
+TESTEDHASH=68ebdf18
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
