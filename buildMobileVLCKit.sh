4ff4950f9b5ee630211b9adc81691ecd7fbfe645
Thomas Guillem <thomas@gllm.fr>
2017-12-08 09:25:17 +0100

    MKV: use vlc git public URL

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -221,7 +221,7 @@ echo `pwd`
 
 if [ "$NONETWORK" != "yes" ]; then
     if ! [ -e vlc ]; then
-        git clone git://git.videolan.org/vlc.git vlc
+        git clone https://git.videolan.org/git/vlc.git vlc
         info "Applying patches to vlc.git"
         cd vlc
         git checkout -B localBranch ${TESTEDHASH}
