4ab481f7c11462cb32b271787bda3cbf2c8a3ae5
Felix Paul Kühne <felix@feepk.net>
2017-12-19 19:47:13 +0100

    Tests: add very basic sample project to debug static builds of MobileVLCKit

---
+++ Tests/iOS/VLCMediaTests/VLCMediaTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VLCMediaTests
+//
+//  Created by Felix Paul Kühne on 19.12.17.
+//  Copyright © 2017 Felix Paul Kühne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
