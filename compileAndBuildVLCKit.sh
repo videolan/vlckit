16ebe9a3c4affe94f815eb1e923f14dd3c12c5ac
Soomin Lee <bubu@mikan.io>
2018-06-22 11:33:47 +0200

    buildscript: Fix tvOS build
    For example, if we choose the compile only aarch64(-a),
    it would go into the simulator part and fail since arm64
    is not a simulator platform.

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -1101,12 +1101,12 @@ if [ "$TVOS" = "yes" ]; then
 
     lipo_libs=""
     platform=""
-    if [ -d ${VLCROOT}/install-AppleTVOS ];then
+    if [ "$FARCH" = "all" ] || (! is_simulator_arch $FARCH);then
         platform="appletvos"
         buildxcodeproj MobileVLCKit "TVVLCKit" ${platform}
         lipo_libs="$lipo_libs ${CONFIGURATION}-appletvos/libTVVLCKit.a"
     fi
-    if [ -d ${VLCROOT}/install-AppleTVSimulator ];then
+    if [ "$FARCH" = "all" ] || (is_simulator_arch $arch);then
         platform="appletvsimulator"
         buildxcodeproj MobileVLCKit "TVVLCKit" ${platform}
         lipo_libs="$lipo_libs ${CONFIGURATION}-appletvsimulator/libTVVLCKit.a"
