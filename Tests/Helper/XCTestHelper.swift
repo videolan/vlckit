ca81ecdd7994bde11ea9476047eb79a68e3807ca
Mike JS. Choi <mkchoi212@icloud.com>
2018-07-11 13:39:50 +0900

    Tests: Add XCTAssertNotNilAndUnwrap

---
+++ Tests/Helper/XCTestHelper.swift
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * XCTestHelper.swift
+ *****************************************************************************
+ * Copyright (C) 2018 Mike JS. Choi
+ * Copyright (C) 2018 VLC authors and VideoLAN
+ * $Id$
+ *
+ * Authors: Mike JS. Choi <mkchoi212 # icloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+
+import XCTest
+
+struct UnexpectedNilError: Error {}
+
+func XCTAssertNotNilAndUnwrap<T>(_ variable: T?, message: String = "Unexpected nil variable", file: StaticString = #file, line: UInt = #line) throws -> T {
+    guard let variable = variable else {
+        XCTFail(message, file: file, line: line)
+        throw UnexpectedNilError()
+    }
+    return variable
+}
+
+let STANDARD_TIME_OUT = 5.0
