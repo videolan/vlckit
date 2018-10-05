0ac40d3da74ea4c6864b55fb24f880a85cb5f186
Soomin Lee <bubu@mikan.io>
2018-10-05 18:15:50 +0200

    Add Swift simple playback project

---
+++ Examples/iOS/SwiftSimplePlayback/SwiftSimplePlayback/Sources/HomeViewController.swift
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ * HomeViewController.swift
+ *
+ * Copyright © 2018 VLC authors and VideoLAN
+ * Copyright © 2018 Videolabs
+ *
+ * Authors: Soomin Lee <bubu@mikan.io>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBAction func startPlayback(_ sender: UIButton) {
+    }
+}
