ab7e9ffd97a8ca20456b14dccc55be9323f151b7
Soomin Lee <bubu@mikan.io>
2018-12-05 11:08:35 +0100

    Bump libvlc hash
    (cherry picked from commit 2be0bef82c506bf002efd0ffcf117311b09f0e4a)
    Signed-off-by: Soomin Lee <bubu@mikan.io>

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="70073ce949" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="218028bb7f" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
