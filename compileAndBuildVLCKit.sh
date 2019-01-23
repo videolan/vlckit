035a03403b240e929580995ca485351ca26b13aa
Felix Paul Kühne <felix@feepk.net>
2019-01-23 20:05:16 +0100

    libvlc: update to merge vout fixes and remove patch merged upstream
    Manual forward-port of a3af5464

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios_version_min
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="5ec99d0db3" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="563b25c5c1" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
