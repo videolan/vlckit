f98ef44bb73f3db0a7227b72fbe3be871a312cc2
Felix Paul Kühne <felix@feepk.net>
2019-11-10 11:50:23 +0900

    libvlc: update to HEAD

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="9db9c1121" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="0a5330890" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
