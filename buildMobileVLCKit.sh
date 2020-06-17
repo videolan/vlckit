bc30380356ca8acdede9ceba843a421b21624abc
Felix Paul Kühne <felix@feepk.net>
2020-06-17 18:00:31 +0200

    libvlc: rebase to include 7df954cb
    This addresses the optimization problem found when linking libebml and fixes #372

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="3915b312a1" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="7df954cb" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
