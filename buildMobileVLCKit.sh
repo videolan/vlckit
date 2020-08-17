914e66d8b9ac8933596f877a70e1d029aa39b947
Felix Paul Kühne <felix@feepk.net>
2020-08-17 17:37:11 +0200

    libvlc: add support for loudness measurements according to EBU R.128
    and rebase libvlc to 3.0 HEAD

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="bccc58cf" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="ef615ff0" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
