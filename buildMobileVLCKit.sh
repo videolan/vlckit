33187bed13d3755ab3bc75c3e01cd9083db8fd10
Felix Paul Kühne <felix@feepk.net>
2020-09-19 13:52:40 +0200

    libvlc: update and fix compilation for tvOS

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="65726cca" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="3d41816e" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
