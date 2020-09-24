120fe5bb0d4ab154b97a97d11defe5495ba291f6
Felix Paul Kühne <felix@feepk.net>
2020-09-25 00:04:10 +0200

    libvlc: fix compilation with iOS/tvOS 14 SDKs

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="3d41816e" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="75b66193" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
