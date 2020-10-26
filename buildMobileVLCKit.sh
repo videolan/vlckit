45f7cf9dcd27f7f7a864edf94f3cb8dd3ec447cd
Felix Paul Kühne <felix@feepk.net>
2020-10-26 16:40:02 +0100

    libvlc: rebase patches and add async media stop support

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="ce04499f" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="bccc58cf" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
