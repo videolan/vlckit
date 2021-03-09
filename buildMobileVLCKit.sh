f8182b96dc47f6debd6e697e7821bfd6612d5ded
Felix Paul Kühne <felix@feepk.net>
2021-03-09 15:59:12 +0100

    libvlc: update SMB and NFS integrations

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -26,7 +26,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="ef615ff0" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="b4c7317a" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
