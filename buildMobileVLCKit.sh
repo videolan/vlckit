d83fc5199f2cd4eaca94c7f66c9996f497d05e26
Soomin Lee <bubu@mikan.io>
2021-10-15 15:38:09 +0200

    buildMobileVLCKit: Bump tested hash
    Removes obsolete ffmpeg backport patch as it has been included in the project.
    
    Closes #532

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="cb1e02fb" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="1f870b73e0" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
