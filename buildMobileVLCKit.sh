e66a26b78ee89c16bac79f71043766f037b6a257
Felix Paul Kühne <felix@feepk.net>
2022-01-04 14:36:24 +0100

    libvlc: update to include SAT>IP fixes

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="ff50ff1e" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="307931a3" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
