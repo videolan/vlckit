8d2a1c148ef8b89f643d9f0e6e3899e68115c2db
Felix Paul Kühne <felix@feepk.net>
2022-03-22 09:11:30 +0100

    libvlc: rebase to upstream

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="426513d8" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="296b3a03" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
