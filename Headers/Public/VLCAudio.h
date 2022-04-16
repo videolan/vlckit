a879ef55e09ffede03510ccfa5a0f2a1fd5baebd
Hank Anderson <ataganak@gmail.com>
2022-04-16 21:37:10 +0900

    VLCAudio :  improve nullability annotations

--- Headers/Public/VLCAudio.h
+++ Headers/Public/VLCAudio.h
@@ -24,6 +24,8 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 /* Notification Messages */
 /**
  * Standard notification messages that are emitted by VLCAudio object.
@@ -61,3 +63,5 @@ OBJC_VISIBLE
 - (void)volumeUp;
 
 @end
+
+NS_ASSUME_NONNULL_END
