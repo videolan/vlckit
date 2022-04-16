5535e9328dadd9fe2abbceb37a96ea82633a9106
Hank Anderson <ataganak@gmail.com>
2022-04-17 02:52:40 +0900

    Supports NS_OPTIONS for VLCMediaParsingOptions

--- Headers/Public/VLCMedia.h
+++ Headers/Public/VLCMedia.h
@@ -524,18 +524,17 @@ NSString *const VLCMediaTracksInformationTypeUnknown;
 
 @property (NS_NONATOMIC_IOSONLY, readonly, copy) NSArray *tracksInformation;
 
-enum {
+/**
+ * enum of available options for use with parseWithOptions
+ * \note you may pipe multiple values for the single parameter
+ */
+typedef NS_OPTIONS(int, VLCMediaParsingOptions) {
     VLCMediaParseLocal          = 0x00,     ///< Parse media if it's a local file
     VLCMediaParseNetwork        = 0x01,     ///< Parse media even if it's a network file
     VLCMediaFetchLocal          = 0x02,     ///< Fetch meta and covert art using local resources
     VLCMediaFetchNetwork        = 0x04,     ///< Fetch meta and covert art using network resources
     VLCMediaDoInteract          = 0x08,     ///< Interact with the user when preparsing this item (and not its sub items). Set this flag in order to receive a callback when the input is asking for credentials.
 };
-/**
- * enum of available options for use with parseWithOptions
- * \note you may pipe multiple values for the single parameter
- */
-typedef int VLCMediaParsingOptions;
 
 /**
  * triggers an asynchronous parse of the media item
