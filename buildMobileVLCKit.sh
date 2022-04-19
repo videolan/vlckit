bd510ebb9fdbfce6827d5ccbdf0298f0f6fd0517
Felix Paul Kühne <felix@feepk.net>
2022-04-19 17:50:27 +0200

    libvlc: rebase 3.0 branch
    This includes the backported patch to support DVB subtitles in mkv and
    therefore fixes #602.

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="cb39c3b5" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="8f6ec51c" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
