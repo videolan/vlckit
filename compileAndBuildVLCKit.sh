3be9dbffa138e97b99c72190c1320cc7f8b0b5f8
Maxime Chapelet <umxprime@videolabs.io>
2022-04-22 12:06:51 +0200

    Change libvlc hash to 69e22f29

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -27,7 +27,7 @@ if [ -z "$MAKEFLAGS" ]; then
     MAKEFLAGS="-j$(sysctl -n machdep.cpu.core_count || nproc)";
 fi
 
-TESTEDHASH="627ab4fe" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="69e22f29" # libvlc hash that this version of VLCKit is build on
 
 usage()
 {
