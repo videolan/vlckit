d74db3070eaab69f4373e8f6fb51d8b3ff3896c9
Maxime Chapelet <umxprime@videolabs.io>
2022-04-26 11:58:49 +0200

    Update source files headers

--- Headers/Public/VLCConsoleLogger.h
+++ Headers/Public/VLCConsoleLogger.h
@@ -1,9 +1,25 @@
-//
-//  VLCConsoleLogger.h
-//  MobileVLCKit
-//
-//  Created by umxprime on 25/04/2022.
-//
+/*****************************************************************************
+ * VLCConsoleLogger.h: [Mobile/TV]VLCKit.framework VLCConsoleLogger header
+ *****************************************************************************
+ * Copyright (C) 2022 VLC authors and VideoLAN
+ * $Id$
+ *
+ * Authors: Maxime Chapelet <umxprime # videolabs.io>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 
 #import <Foundation/Foundation.h>
 
