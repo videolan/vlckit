901393f0097db3cb5aa772c0027f509ac1e61430
Felix Paul Kühne <felix@feepk.net>
2022-09-13 10:14:30 +0200

    libvlc: update hash and patches

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -25,7 +25,7 @@ OSVERSIONMINLDFLAG=ios
 ROOT_DIR=empty
 FARCH="all"
 
-TESTEDHASH="5c8512df" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="7c166c14" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
