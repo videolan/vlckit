ddefa7c20a732d706622dbc5524cea566cdabb03
Felix Paul Kühne <felix@feepk.net>
2022-12-12 15:41:10 +0100

    VLCKit.h: expose VLCDialogProvider
    The class and the associated protocol was previously not exposed on
    macOS as part of the umbrella header while the header itself was
    shipped, so the API was hidden from swift relying on the module map.
    
    Additionally, this reorders the header content to make it easier to
    compare with the iOS counterpart.
    
    Forward-port of a7d97dd7

--- Headers/Public/VLCKit.h
+++ Headers/Public/VLCKit.h
@@ -2,10 +2,10 @@
  * VLCKit.h: VLCKit.framework main header
  *****************************************************************************
  * Copyright (C) 2007 Pierre d'Herbemont
- * Copyright (C) 2007, 2013-2014 VLC authors and VideoLAN
- * $Id$
+ * Copyright (C) 2007, 2013-2022 VLC authors and VideoLAN
  *
  * Authors: Pierre d'Herbemont <pdherbemont # videolan.org>
+ *          Felix Paul Kühne <fkuehne # videolan.org
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -22,13 +22,17 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#import <VLCKit/VLCAudio.h>
 #import <VLCKit/VLCLibrary.h>
 #import <VLCKit/VLCMedia.h>
-#import <VLCKit/VLCMediaList.h>
-#import <VLCKit/VLCMediaListPlayer.h>
 #import <VLCKit/VLCMediaDiscoverer.h>
+#import <VLCKit/VLCMediaLibrary.h>
+#import <VLCKit/VLCMediaList.h>
 #import <VLCKit/VLCMediaPlayer.h>
+#import <VLCKit/VLCMediaListPlayer.h>
 #import <VLCKit/VLCMediaThumbnailer.h>
+#import <VLCKit/VLCMediaMetaData.h>
+#import <VLCKit/VLCDialogProvider.h>
 #import <VLCKit/VLCStreamSession.h>
 #import <VLCKit/VLCStreamOutput.h>
 #import <VLCKit/VLCTime.h>
@@ -39,21 +43,27 @@
 #import <VLCKit/VLCRendererItem.h>
 #import <VLCKit/VLCFilter.h>
 #import <VLCKit/VLCAdjustFilter.h>
-#import <VLCKit/VLCMediaMetaData.h>
 #import <VLCKit/VLCLogging.h>
-#import <VLCKit/VLCFileLogger.h>
 #import <VLCKit/VLCConsoleLogger.h>
+#import <VLCKit/VLCFileLogger.h>
 #import <VLCKit/VLCLogMessageFormatter.h>
-#import <VLCKit/VLCAudio.h>
 
 @class VLCMedia;
+@class VLCMediaLibrary;
 @class VLCMediaList;
 @class VLCTime;
 @class VLCVideoView;
+@class VLCAudio;
+@class VLCMediaThumbnailer;
+@class VLCMediaListPlayer;
+@class VLCMediaPlayer;
 @class VLCMediaMetaData;
-@class VLCFileLogger;
-@class VLCConsoleLogger;
-@class VLCLogMessageFormatter;
-@class VLCFilterParameterValue;
+@class VLCDialogProvider;
+@class VLCRendererDiscoverer;
+@class VLCRendererDiscovererDescription;
+@class VLCRendererItem;
 @class VLCFilterParameter;
 @class VLCAdjustFilter;
+@class VLCConsoleLogger;
+@class VLCFileLogger;
+@class VLCLogMessageFormatter;
