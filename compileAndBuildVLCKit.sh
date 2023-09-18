5cf938f017e8285f893ffa222d8fb7dca50740be
Felix Paul Kühne <felix@feepk.net>
2023-09-18 09:54:15 +0200

    libvlc: update to e6b30a03
    This adds the new vout based on AVSampleBufferDisplayLayer.

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -28,7 +28,7 @@ if [ -z "$MAKEFLAGS" ]; then
     MAKEFLAGS="-j$(sysctl -n machdep.cpu.core_count || nproc)";
 fi
 
-TESTEDHASH="db063d7f" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="e6b30a03" # libvlc hash that this version of VLCKit is build on
 
 usage()
 {
