289bb628bca0d0b20e37a4ff71ae6bd09383df74
Felix Paul Kühne <felix@feepk.net>
2024-03-03 16:46:41 +0100

    libvlc: update hash
    This fixes #456

--- buildMobileVLCKit.sh
+++ buildMobileVLCKit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Copyright (C) Pierre d'Herbemont, 2010
-# Copyright (C) Felix Paul Kühne, 2012-2023
+# Copyright (C) Felix Paul Kühne, 2012-2024
 
 set -e
 
@@ -26,7 +26,7 @@ ROOT_DIR=empty
 INCLUDE_32BIT=no
 FARCH="all"
 
-TESTEDHASH="4733d1cc" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="b7843ec8" # libvlc hash that this version of VLCKit is build on
 
 if [ -z "$MAKE_JOBS" ]; then
     CORE_COUNT=`sysctl -n machdep.cpu.core_count`
