0eb9f07587c7a5f156b734afc87fca0b79d615e4
Felix Paul Kühne <fkuehne@videolan.org>
2024-03-19 11:34:31 +0100

    VLCMedia: update parsing options to match libvlc

--- Headers/Public/Media/VLCMedia.h
+++ Headers/Public/Media/VLCMedia.h
@@ -305,11 +305,12 @@ typedef NS_ENUM(int, VLCMediaFileStatReturnType) {
  * \note you may pipe multiple values for the single parameter
  */
 typedef NS_OPTIONS(int, VLCMediaParsingOptions) {
-    VLCMediaParseLocal          = 0x00,     ///< Parse media if it's a local file
-    VLCMediaParseNetwork        = 0x01,     ///< Parse media even if it's a network file
-    VLCMediaFetchLocal          = 0x02,     ///< Fetch meta and covert art using local resources
-    VLCMediaFetchNetwork        = 0x04,     ///< Fetch meta and covert art using network resources
-    VLCMediaDoInteract          = 0x08,     ///< Interact with the user when preparsing this item (and not its sub items). Set this flag in order to receive a callback when the input is asking for credentials.
+    VLCMediaParseLocal          = 0x01,     ///< Parse media if it's a local file
+    VLCMediaParseNetwork        = 0x02,     ///< Parse media even if it's a network file
+    VLCMediaParseForced         = 0x04,     ///< Force parsing the media even if it would be skipped
+    VLCMediaFetchLocal          = 0x08,     ///< Fetch meta and cover art using local resources
+    VLCMediaFetchNetwork        = 0x10,     ///< Fetch meta and cover art using network resources
+    VLCMediaDoInteract          = 0x20,     ///< Interact with the user when preparsing this item (and not its sub items). Set this flag in order to receive a callback when the input is asking for credentials.
 };
 
 /**
