34075e75a139cc29e6507f756240886b5f611839
Hank Anderson <ataganak@gmail.com>
2024-04-15 06:13:07 +0900

    VLCLibrary: Add currentErrorMessage class property

--- Headers/Public/VLCLibrary.h
+++ Headers/Public/VLCLibrary.h
@@ -44,6 +44,16 @@ OBJC_VISIBLE
 @property (class, nonatomic, nullable) id<VLCEventsConfiguring> sharedEventsConfiguration;
 
 /**
+ * A human-readable error message for the last LibVLC error in the calling
+ * thread. The resulting string is valid until another error occurs (at least
+ * until the next LibVLC call).
+ *
+ * @warning
+ * This will be NULL if there was no error.
+ */
+@property (class, nonatomic, copy, nullable) NSString *currentErrorMessage;
+
+/**
  * Returns the library's shared instance
  * \return The library's shared instance
  */
