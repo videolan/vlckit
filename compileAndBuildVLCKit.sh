9c907fd153d69dbc4d381d6492cbbeff6321d16a
Felix Paul Kühne <felix@feepk.net>
2024-06-04 14:47:09 +0200

    libvlc: use new AVSampleBuffer aout on xrOS

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -29,7 +29,7 @@ if [ -z "$MAKEFLAGS" ]; then
     MAKEFLAGS="-j$(sysctl -n machdep.cpu.core_count || nproc)";
 fi
 
-TESTEDHASH="8f09a0c4" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="d1e64ef2" # libvlc hash that this version of VLCKit is build on
 
 usage()
 {
