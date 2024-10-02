4f1a3398ac858912b5c676cedd5f28f51382ff7d
Maxime Chapelet <umxprime@videolabs.io>
2024-10-02 10:35:14 +0200

    libvlc: update to latest changes
    This lidvlc update is mandatory to support on_seek events

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -30,7 +30,7 @@ if [ -z "$MAKEFLAGS" ]; then
     MAKEFLAGS="-j$(sysctl -n machdep.cpu.core_count || nproc)";
 fi
 
-TESTEDHASH="ceb1cef1" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="1b395032" # libvlc hash that this version of VLCKit is build on
 
 usage()
 {
