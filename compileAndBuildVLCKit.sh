564cf7df5ebe371f280a41133494f591c6f8e31d
Felix Paul Kühne <felix@feepk.net>
2024-11-22 16:03:44 +0100

    libvlc: update hash and patchset
    This solves timing issues among other things

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -30,7 +30,7 @@ if [ -z "$MAKEFLAGS" ]; then
     MAKEFLAGS="-j$(sysctl -n machdep.cpu.core_count || nproc)";
 fi
 
-TESTEDHASH="1b395032" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="2f08e3cc" # libvlc hash that this version of VLCKit is build on
 
 usage()
 {
