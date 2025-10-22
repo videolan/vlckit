c4acea2ae9e682ba4b216af297ae30bc179815e2
Martin Finkel <me@martinfinkel.com>
2025-10-22 13:21:57 +0700

    bump hash

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -30,7 +30,7 @@ if [ -z "$MAKEFLAGS" ]; then
     MAKEFLAGS="-j$(sysctl -n machdep.cpu.core_count || nproc)";
 fi
 
-TESTEDHASH="5e065cfa0" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="05f437f98" # libvlc hash that this version of VLCKit is build on
 
 usage()
 {
