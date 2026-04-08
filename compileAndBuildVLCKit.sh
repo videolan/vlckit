b671b9f8b78c9a8c64d39a13396622c705c5dddc
Felix Paul Kühne <felix@feepk.net>
2026-04-08 18:03:47 +0200

    libvlc: update and fix json symbol duplication

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -30,7 +30,7 @@ if [ -z "$MAKEFLAGS" ]; then
     MAKEFLAGS="-j$(sysctl -n machdep.cpu.core_count || nproc)";
 fi
 
-TESTEDHASH="c833c4be0" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="85a537d69" # libvlc hash that this version of VLCKit is build on
 
 usage()
 {
